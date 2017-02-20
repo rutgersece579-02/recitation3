6e283fdb51936a18f0bcaad8f099b933375b768b
rutgersece579-02 <appliedparallelrutgers17@gmail.com>
2017-02-19 20:00:02 -0500

    Update pi.cpp

--- pi.cpp
+++ pi.cpp
@@ -23,19 +23,14 @@ int main(int argc, char **argv)
   long int i, intervals = INTERVALS;
   double x, dx, f, sum, pi;
   double time2;
-#ifdef _OPENMP
-  double time1 = omp_get_wtime();
-#else
+
   time_t time1 = clock();
-#endif
 
   printf("Number of intervals: %ld\n", intervals);
 
   sum = 0.0;
   dx = 1.0 / (double) intervals;
-
-#pragma omp parallel for private(x,f) \
-  reduction(+:sum)
+  
   for (i = 1; i <= intervals; i++) {
     x = dx * ((double) (i - 0.5));
     f = 4.0 / (1.0 + x*x);
@@ -44,11 +39,8 @@ int main(int argc, char **argv)
 
   pi = dx*sum;
 
-#ifdef _OPENMP
-  time2 = omp_get_wtime() - time1;
-#else
   time2 = (clock() - time1) / (double) CLOCKS_PER_SEC;
-#endif
+
 
   printf("Computed PI %.24f\n", pi);
   printf("The true PI %.24f\n\n", PI25DT);
